50475d45060c718656dd432ee96b10ad10b8c4d1
theliquidspoonyone <yolo@yolo.com>
2021-01-09 03:35:20 +0000

    Updates

---
+++ precache-manifest.20904c4804b53e3d146442fcc2e2947b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaac599320058827daf7d9967f189f0",
+    "url": "/spoony-sad-tweet-generator/index.html"
+  },
+  {
+    "revision": "b1c114f39f0be6f06efe",
+    "url": "/spoony-sad-tweet-generator/static/css/main.f9de32ef.chunk.css"
+  },
+  {
+    "revision": "76c0391edb561f98675f",
+    "url": "/spoony-sad-tweet-generator/static/js/2.5eb106fd.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/spoony-sad-tweet-generator/static/js/2.5eb106fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c114f39f0be6f06efe",
+    "url": "/spoony-sad-tweet-generator/static/js/main.7f66f356.chunk.js"
+  },
+  {
+    "revision": "cf8d30cd942e939895bf",
+    "url": "/spoony-sad-tweet-generator/static/js/runtime-main.d20b79b4.js"
+  }
+]);
